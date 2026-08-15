2bf596e19b4a96f0974a11740dd4ca08f75808ef
DevenLall <devenlall05@gmail.com>
2026-08-15 12:51:52 -0400

    added docker connection and the chat django folder

--- _core/settings.py
+++ _core/settings.py
@@ -91,8 +91,16 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = '_core.wsgi.application'
+ASGI_APPLICATION = '_core.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
---
+++ a_chat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AChatConfig(AppConfig):
+    name = 'a_chat'
