1cfde0da78b68e4e99cd1fd3acc8a33cce0bea38
DevenLall <devenlall05@gmail.com>
2026-08-15 16:28:14 -0400

    finished the communication, browse room works, and creating a new room works

--- _core/asgi.py
+++ _core/asgi.py
@@ -1,16 +1,24 @@
 """
 ASGI config for _core project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+Routes plain HTTP requests to Django as normal, and WebSocket connections
+to our Channels consumers (defined in a_chat/routing.py).
 """
-
 import os
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from a_chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
--- _core/settings.py
+++ _core/settings.py
@@ -95,7 +95,7 @@ ASGI_APPLICATION = '_core.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'BACKEND': 'channels_redis.pubsub.RedisPubSubChannelLayer',
         'CONFIG': {
             "hosts": [('127.0.0.1', 6379)],
         },
